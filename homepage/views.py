dc242131569c012c2bd6a8c807eea583ec1346f5
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-02-19 02:18:15 -0800

    added homepage functionality

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
+def process(request, text):
+    if text == "hi":
+        return "hello"
+    if text == "who are you":
+        return "im the mystical cat of you're dreams"
+    if text == "its not you're its your":
+        return "its not YOUR business. Mind your own."
+    if text == "im sad":
+        return "lol"
+    if text == "what lol":
+        return "hehe"
+    else:
+        return "I didnt understand"
+
+
 def homepage(request):
-    return render(request, "home.html")
+    context = {}
+
+    if request.method == "POST":
+        text = request.POST["inp"]
+        context.setdefault("text", process(request, text))
+
+    return render(request, "home.html", context)
--- mycp/settings.py
+++ mycp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +86,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [ 
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../static"))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
