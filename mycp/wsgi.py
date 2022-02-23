4739f881405ea29d78a5b600d2f19ded0a154454
vbobus <varghesebobus@gmail.com>
2022-02-23 13:06:11 -0800

    deployment updates

--- mycp/wsgi.py
+++ mycp/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+# code for deployment
+
+import os  
 
 from django.core.wsgi import get_wsgi_application
 
